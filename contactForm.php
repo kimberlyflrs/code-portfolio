98e3c0bc389c4d4dc63a9dbe053dd90d5ca34d70
Kimberly Flores <kimberlyflrs@users.noreply.github.com>
2020-08-03 22:42:41 -0700

    working on contact form

---
+++ contactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $message = $_POST['message'];
+    $subject = "Website Message"
+
+    $mailTo = "kimberly.marleny@gmail.com";
+    $headers = "From: ".$mail;
+    $text = "You have received a message from ".$name.".\n\n.$message;
+
+    mail($mailTo, $subject, $text, $headers);
+    header("Location: index.html?mailsend");
+
+}
