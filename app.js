b131a850bb692000845433b04652ec5dd2c9b5c6
Kimberly Flores <kimberlyflrs@users.noreply.github.com>
2020-08-11 23:16:00 -0700

    updated project page

--- app.js
+++ app.js
@@ -24,6 +24,65 @@ const navSlide = ()=>{
     })
 }
 
+/*Filtering elements on the project page */
+
+const filterProjects = (project) => {
+    var projects = document.getElementsByClassName('filterProject');
+    for(var i=0; i<projects.length;i++){
+        /*Remove the show class */
+        removeClass (projects[i], "show");
+        if(projects[i].className.indexOf(project)>-1){
+            /*Add the show class to it */
+            addClass(projects[i], "show");
+        }
+    }
+    activeBtn(project);
+}
+
+const removeClass = (project, name) =>{
+    project.className = project.className.replace(name, "");
+}
+
+const addClass = (project, name) =>{
+    project.className += " "+name; 
+}
+
+const activeBtn = (project) =>{
+    //adds active to filter btn, deletes other not active
+    var filterBtns = document.getElementsByClassName('filter-btn');
+    if(project === "filterProject"){
+        project = "all";
+    }
+    for(var i=0; i<filterBtns.length;i++){
+        /*Remove the show class */
+        removeClass(filterBtns[i], "active-btn");
+        if(filterBtns[i].className.indexOf(project)>-1){
+            addClass(filterBtns[i], "active-btn");
+        }
+    }    
+}
+
+
+
+/*const scroll = () =>{
+    //make items visible on scroll
+    var items = document.querySelectorAll('.hide');
+    console.log(items);
+    for (var i=0; i<items.length;i++){
+        console.log(items[i]);
+        var bottom_object = items[i].position().top + items[i].outerHeight();
+        var bottom_window = window.scrollTop()+window.innerHeight();
+
+        console.log(bottom_object);
+        console.log(bottom_window);
+    }
+}*/
+
 window.onload=function(){
     navSlide();
+    filterProjects("filterProject");
 }
+
+/*window.onscroll=function(){
+    scroll();
+}*/
